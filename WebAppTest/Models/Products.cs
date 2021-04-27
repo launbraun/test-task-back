9bbcceb34c2e0787836457cd8264772a35b3cd21
launbraun <launbraun@bk.ru>
2021-04-27 17:33:23 +0300

    deploy

---
+++ WebAppTest/Models/Products.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace WebAppTest.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string header { get; set; }
+        public string description { get; set; }
+        public IList<string> route { get; set; }
+        public DateTimeOffset periodStart { get; set; }
+        public DateTimeOffset periodEnd { get; set; }
+        public double minPrice { get; set; }
+        public PhotoCard photoCard { get; set; }
+        public IList<PhotoAlbum> photoAlbum { get; set; }
+    }
+
+    public class PhotoCard
+    {
+        public string photo { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+    public class PhotoAlbum
+    {
+        public string photo { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+}
+
+
